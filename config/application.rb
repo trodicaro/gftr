5b2707300cdedfc08f29f9057cdf7908536f4c26
trodicaro <trodicaro@gmail.com>
2015-11-15 01:03:52 -0600

    Precompile assets locally

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,9 @@ Bundler.require(*Rails.groups)
 
 module Giftr
   class Application < Rails::Application
+
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -22,9 +25,9 @@ module Giftr
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.assets.initialize_on_precompile = false
 
   end
 end
