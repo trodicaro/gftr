88e3d8c3d6d2a9f4b1f20b0ec1a8671cc4047187
Dalal Alrayes <dalrayes@gmail.com>
2015-11-25 16:00:49 -0600

    Fixes mailer functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
     root 'pages#index'
-    get '/circles', to: 'pages#circles'
-    get '/planning', to: 'pages#planning'
+   
     post '/users', to: 'users#create'
   end
