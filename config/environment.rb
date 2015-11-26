a6b5c4a299d577a0de4f73762459f663b628ff90
Dalal Alrayes <dalrayes@gmail.com>
2015-11-26 13:11:54 -0600

    Adds ENV settings for action mailer

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,13 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-
+app_env_vars = File.join(Rails.root, 'config', 'initializers', 'app_env_vars.rb')
+load(app_env_vars) if File.exists?(app_env_vars)
 # Initialize the Rails application.
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => "givegiftr",
-  :password => "givegiftr8",
+  :user_name => ENV["EMAIL"],
+  :password => ENV["PASSWORD"],
   :domain => "givegiftr.co",
   :address => "smtp.sendgrid.net",
   :port => 587, 
