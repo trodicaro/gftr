cbb74b687d36b8ffce0445536fa2cb3b4c9a5bbf
Dalal Alrayes <dalrayes@users.noreply.github.com>
2015-11-25 18:41:51 -0600

    Revert "Fixes mailer functionality"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,2 @@
 class User < ActiveRecord::Base
-	validates :email, presence: true
-	# validates :email, uniqueness: true
-	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     root 'pages#index'
-   
+    get '/circles', to: 'pages#circles'
+    get '/planning', to: 'pages#planning'
     post '/users', to: 'users#create'
   end
