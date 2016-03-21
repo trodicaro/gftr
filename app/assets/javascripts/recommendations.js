43adbabdc3b8219599d2004eef400ed519ee3cc0
Dalal Alrayes <dalrayes@gmail.com>
2016-03-21 17:54:55 -0500

    Adds show/hide jquery and google analytics

--- app/assets/javascripts/recommendations.js
+++ app/assets/javascripts/recommendations.js
@@ -1,11 +1,28 @@
 $(document).ready(function(){
 	$('#save-options').click(function(){
 		$('#userform').show();
+		$('#hello_message').hide();
 	});
 
 
 	$('#more-help').click(function(){
+		$('#more-help').hide();
 		$('#more-options').show();
 	});
 
+
+	$('#rerun-algorithm').click(function(){
+		$('#more-options').hide();
+		$('#thank-you').show();
+	});
+
+	$('#gift-concierge').click(function(){
+		$('#more-options').hide();
+		$('#thank-you').show();
+	});	
+
+	$('#second-opinion').click(function(){
+		$('#more-options').hide();
+		$('#thank-you').show();
+	});	
 })
