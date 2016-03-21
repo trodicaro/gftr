6b241ec6c85e52f9e4a4c32ea548e840a5d811c3
Dalal Alrayes <dalrayes@gmail.com>
2016-03-21 09:56:11 -0500

    Adds show/hide to sign up and next steps

---
+++ app/assets/javascripts/recommendations.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('#save-options').click(function(){
+		$('#userform').show();
+	});
+
+
+	$('#more-help').click(function(){
+		$('#more-options').show();
+	});
+
+})
