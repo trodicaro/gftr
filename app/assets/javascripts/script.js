08481d15e78055b13b70e9cce1693d7d520e1ff7
Tatyana Shestopalova <tshestopalova@gmail.com>
2016-03-24 21:22:35 -0500

    timer animation

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -49,38 +49,12 @@ function scrollTowards(el) {
   }, 400, 'swing');
 };
 
-//COUNTDOWN
+//BEGIN COUNTDOWN
 $(function(){
-//  console.log(new Date().getTime());
-//	var note = $('#note'),
-//		ts = new Date(2017, 0, 1),
 		ts = new Date().getTime() + 10*60*1000;//add 10 minutes
-//		newYear = true;
-
-//	if((new Date()) > ts){
-//		// The new year is here! Count towards something else.
-//		// Notice the *1000 at the end - time must be in milliseconds
-////		ts = (new Date()).getTime() + 10*24*60*60*1000;
-//
-////		newYear = false;
-//	}
 	$('#countdown').countdown({
 		timestamp	: ts,
-//		callback	: function(days, hours, minutes, seconds){
 		callback	: function(minutes, seconds){
-//			var message = "";
-//
-//			message += days + " day" + ( days==1 ? '':'s' ) + ", ";
-//			message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
-//			message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
-//			message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
-//			if(newYear){
-//				message += "left until the new year!";
-//			}
-//			else {
-//				message += "left to 10 days from now!";
-//			}
-//			note.html(message);
 		}
 	});
 
@@ -102,7 +76,6 @@ $(function(){
 			timestamp	: 0
 		},prop);
 
-//		var left, d, h, m, s, positions;
 		var left, m, s, positions;
 
 		// Initialize the plugin
@@ -113,22 +86,11 @@ $(function(){
 		(function tick(){
 
 			// Time left
-//			left = Math.floor((options.timestamp - (new Date())) / 1000);
 			left = Math.floor((options.timestamp - (new Date())) / 1000);
 
 			if(left < 0){
 				left = 0;
 			}
-
-			// Number of days left
-//			d = Math.floor(left / days);
-//			updateDuo(0, 1, d);
-//			left -= d*days;
-
-			// Number of hours left
-//			h = Math.floor(left / hours);
-//			updateDuo(2, 3, h);
-//			left -= h*hours;
 
 			// Number of minutes left
 			m = Math.floor(left / minutes);
@@ -140,7 +102,6 @@ $(function(){
 			updateDuo(6, 7, s);
 
 			// Calling an optional user supplied callback
-//			options.callback(d, h, m, s);
 			options.callback(m, s);
 
 			// Scheduling another call of this function in 1s
@@ -158,36 +119,6 @@ $(function(){
 
 function init(elem, options){
 		elem.addClass('countdownHolder');
-
-		// Creating the markup inside the container
-//		$.each(['Days','Hours','Minutes','Seconds'],function(i){
-//			$('<span class="count'+this+'">').html(
-//				'<span class="position">\
-//					<span class="digit static">0</span>\
-//				</span>\
-//				<span class="position">\
-//					<span class="digit static">0</span>\
-//				</span>'
-//			).appendTo(elem);
-//
-//			if(this!="Seconds"){
-//				elem.append('<span class="countDiv countDiv'+i+'"></span>');
-//			}
-//		});
-//        $.each(['Days','Hours','Minutes','Seconds'],function(i){
-//			$('<span class="count'+this+'">').html(
-//				'<span class="position">\
-//					<span class="digit static">0</span>\
-//				</span>\
-//				<span class="position">\
-//					<span class="digit static">0</span>\
-//				</span>'
-//			).appendTo(elem);
-//
-//			if(this!="Seconds"){
-//				elem.append('<span class="countDiv countDiv'+i+'"></span>');
-//			}
-//		});
 
 	}
 
@@ -210,8 +141,8 @@ function init(elem, options){
 		var replacement = $('<div>',{
 			'class':'digit',
 			css:{
-				top:'-2.1em',
-				opacity:0
+//				top:'-2.1em',
+//				opacity:1
 			},
 			html:number
 		});
@@ -222,7 +153,7 @@ function init(elem, options){
 		digit
 			.before(replacement)
 			.removeClass('static')
-			.animate({top:'2.5em',opacity:0},'fast',function(){
+			.animate('fast',function(){
 				digit.remove();
 			})
 
@@ -233,3 +164,4 @@ function init(elem, options){
 			});
 	}
 })(jQuery);
+//END COUNTDOWN
