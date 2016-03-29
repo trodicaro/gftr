4fd642fbb33e5c9a5d58e1a45f32677c3c4474b0
Dalal Alrayes <dalrayes@gmail.com>
2016-03-29 17:06:42 -0500

    refactors javascript for analytics on don't panic

--- app/assets/javascripts/recommendations.js
+++ app/assets/javascripts/recommendations.js
@@ -1,12 +1,30 @@
 var resizeListener;
+
 function myfunction() {
-  clearTimeout(resizeListener);
-  resizeListener = setTimeout(function(){
-    $('.savebar-push').each(function() {
-      $(this).css("height", "");
-      $(this).height($('.savebar').height());
+    clearTimeout(resizeListener);
+    resizeListener = setTimeout(function() {
+        $('.savebar-push').each(function() {
+            $(this).css("height", "");
+            $(this).height($('.savebar').height());
+        });
     });
-  });
 }
 
 $(window).resize(myfunction);
+
+$(function() {
+
+    function sayHi(name) {
+        console.log("hi " + name);
+    };
+
+    function sendAnalytics(eventCategory, eventAction) {
+    	ga('send', 'event', eventCategory, eventAction);
+    };
+
+    $('#click-panic').on("click", function(event) {
+        event.preventDefault();
+        sendAnalytics('Help', 'moreHelp');
+        sayHi('dalal');
+    });
+});
