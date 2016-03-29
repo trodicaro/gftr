e96cc34076fa3971dc7b6e805f0eb1dadb7ce92a
Dalal Alrayes <dalrayes@gmail.com>
2016-03-29 12:34:18 -0500

    Updates thank yous

---
+++ db/migrate/20160328213112_create_visitors.rb
@@ -0,0 +1,10 @@
+class CreateVisitors < ActiveRecord::Migration
+  def change
+    create_table :visitors do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.timestamps
+    end
+  end
+end
