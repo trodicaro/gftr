6dc974fd6b6de1c4cc29f0f50dc5485d29389aeb
Tatyana Shestopalova <tshestopalova@gmail.com>
2016-03-29 20:43:25 -0500

    adds gift idea counter

--- app/assets/javascripts/recommendations.js
+++ app/assets/javascripts/recommendations.js
@@ -10,3 +10,14 @@ function myfunction() {
 }
 
 $(window).resize(myfunction);
+
+var foo;
+
+jQuery(document).ready(function($) {
+  $('input[type="checkbox"]').click(function() {
+//    console.log("woohoo");
+    $('#test2').text(document.querySelectorAll('input[type="checkbox"]:checked').length);
+//    console.log(document.querySelectorAll('input[type="checkbox"]:checked').length);
+//    foo = document.querySelectorAll('input[type="checkbox"]:checked').length
+  });
+});
