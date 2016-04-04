74f0adf6ffd9803c6bc6fd032207b32bdd7d9461
Dalal Alrayes <dalrayes@gmail.com>
2016-04-04 17:01:23 -0500

    Updates links to thumbnails and IDs

--- app/assets/javascripts/buylinks.js
+++ app/assets/javascripts/buylinks.js
@@ -2,53 +2,52 @@
 
 $(function() {
   //idea1
-  $("#ONE:BuyIdea1_Option1").click(function() {
-    // window.open("http://www.google.com",'_blank');
-    console.log("this sucks");
-  });
-  
-  $("#ONE:BuyIdea1_Option2").click(function() {
+  $("#ONEBuyIdea1_Option1").click(function() {
     window.open("http://www.google.com",'_blank');
   });
   
-  $("#ONE:BuyIdea1_Option3").click(function() {
+  $("#ONEBuyIdea1_Option2").click(function() {
     window.open("http://www.google.com",'_blank');
   });
   
-  $("#ONE:BuyIdea1_Option4").click(function() {
+  $("#ONEBuyIdea1_Option3").click(function() {
+    window.open("http://www.google.com",'_blank');
+  });
+  
+  $("#ONEBuyIdea1_Option4").click(function() {
     window.open("http://www.google.com",'_blank');
   });
   
   //idea2
-  $("#ONE:BuyIdea2_Option1").click(function() {
+  $("#ONEBuyIdea2_Option1").click(function() {
     window.open("http://www.google.com",'_blank');
   });
   
-  $("#ONE:BuyIdea2_Option2").click(function() {
+  $("#ONEBuyIdea2_Option2").click(function() {
     window.open("http://www.google.com",'_blank');
   });
   
-  $("#ONE:BuyIdea2_Option3").click(function() {
+  $("#ONEBuyIdea2_Option3").click(function() {
     window.open("http://www.google.com",'_blank');
   });
   
-  $("#ONE:BuyIdea2_Option4").click(function() {
+  $("#ONEBuyIdea2_Option4").click(function() {
     window.open("http://www.google.com",'_blank');
   });
   //idea3
-  $("#ONE:BuyIdea3_Option1").click(function() {
+  $("#ONEBuyIdea3_Option1").click(function() {
     window.open("http://www.google.com",'_blank');
   });
   
-  $("#ONE:BuyIdea3_Option2").click(function() {
+  $("#ONEBuyIdea3_Option2").click(function() {
     window.open("http://www.google.com",'_blank');
   });
   
-  $("#ONE:BuyIdea3_Option3").click(function() {
+  $("#ONEBuyIdea3_Option3").click(function() {
     window.open("http://www.google.com",'_blank');
   });
   
-  $("#ONE:BuyIdea3_Option4").click(function() {
+  $("#ONEBuyIdea3_Option4").click(function() {
     window.open("http://www.google.com",'_blank');
   });
   
