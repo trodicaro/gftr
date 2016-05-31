ada2fb31af3578285d1d07f34a2bd3fce9e2d74e
Dalal Alrayes <dalrayes@gmail.com>
2016-05-31 17:31:06 -0500

    updates sign up and analytics

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,13 @@
+   function sendAnalytics(eventCategory, eventAction) {
+       ga('send', 'event', eventCategory, eventAction, eventLabel);
+   };
+
+   $('#join').on("click", function(event) {
+       event.preventDefault();
+       sendAnalytics('Interactions', 'clicked', 'Join the Fun');
+   });
+
+   $('#about-us').on("click", function(event) {
+       event.preventDefault();
+       sendAnalytics('Interactions', 'clicked', 'About Us');
+   });
