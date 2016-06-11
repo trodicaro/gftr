b4a4afe233b1ac2dc0f5d1d423a272c142b5fe0a
Dalal Alrayes <dalrayes@gmail.com>
2016-06-11 00:08:58 -0500

    Updates links with analytics

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -4,21 +4,17 @@ $(function() {
     };
 
     $('#joinsection').on("click", function(event) {
-        event.preventDefault();
         sendAnalytics('Interactions', 'clicked', 'Nav: Join the Fun');
     });
 
     $('#join-exist').on("click", function(event) {
-        event.preventDefault();
         sendAnalytics('Interactions', 'clicked', 'Join the Fun1');
     });
     $('#join-group').on("click", function(event) {
-        event.preventDefault();
         sendAnalytics('Interactions', 'clicked', 'Join the Fun2');
     });
 
     // $('#about-us').on("click", function(event) {
-    //     event.preventDefault();
     //     sendAnalytics('Interactions', 'clicked', 'About Us');
     // });
 });
